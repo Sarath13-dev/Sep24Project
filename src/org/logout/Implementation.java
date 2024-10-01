548a8fd3c53b5206933a24ba44089ccb4cfa8bb8
Sarath13-dev <ramsssjrs@gmail.com>
2024-10-01 09:30:16 +0530

    QA

--- src/org/logout/Implementation.java
+++ src/org/logout/Implementation.java
@@ -2,6 +2,12 @@ package org.logout;
 
 public class Implementation {
 	
+	public static void main(String[] args) {
+		
+		System.out.println("Work done by QA");
+		System.out.println("It is running fine");
+		
+	}
 	
 
 }
